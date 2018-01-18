5413ae7ada93800472168a656a890f9738c6fa99
zhujinxuan <zhujinxuan@gmail.com>
2018-01-18 16:07:15 -0500

    Keep startKey and endKey of selection when deleteAtRange

--- tests/delete-at-range-between-two-bad-cells-with-selection/change.js
+++ tests/delete-at-range-between-two-bad-cells-with-selection/change.js
@@ -5,12 +5,11 @@ export default function(plugin, change) {
     const { document } = change.value;
     const anchorBlock = document.getDescendant('_anchor_');
     const focusBlock = document.getDescendant('_focus_');
+    change.collapseToStartOf(anchorBlock).extendToEndOf(focusBlock);
     plugin.changes.deleteAtRange(
         change,
-        Range.create()
-            .moveAnchorToEndOf(anchorBlock)
-            .moveFocusToStartOf(focusBlock)
-            .moveFocus(1)
+        change.value.selection,
+        { normalize: false }
     );
     return change;
 }
