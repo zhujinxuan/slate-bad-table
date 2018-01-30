1ac3a568e852fb5f107612f420a04064d866dfb1
zhujinxuan <zhujinxuan@gmail.com>
2018-01-30 15:21:25 -0500

    Confirmed deleteAtRange

---
+++ tests/delete-at-range-with-edit-table-no-delete-endText/change.js
@@ -0,0 +1,13 @@
+// import expect from 'expect';
+
+export default function(plugin, change) {
+    const { document } = change.value;
+    const anchorBlock = document.getDescendant('_anchor_');
+    const focusBlock = document.getDescendant('_focus_');
+    change.collapseToStartOf(anchorBlock).extendToEndOf(focusBlock);
+    plugin.changes.deleteAtRange(change, change.value.selection, {
+        normalize: false,
+        deleteEndText: false
+    });
+    return change;
+}
